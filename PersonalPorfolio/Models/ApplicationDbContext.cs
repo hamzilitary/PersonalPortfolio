ed95e2f325f0a886b94c29f0f7481a5e3116469f
Kayla Ondracek and Johnny Mayer <kayla.renee@gmail.com and johnnymayer@gmail.com>
2018-05-04 11:39:26 -0700

    add initial files and steup up migrations with User Auth

---
+++ PersonalPorfolio/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalPorfolio.Models
+{
+    public class ApplicationDbContext
+    {
+    }
+}
